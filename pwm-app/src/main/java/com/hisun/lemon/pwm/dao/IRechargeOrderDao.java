66489a8eaff8d4e4111e8d2a026ef7494cacfaa2
冼黎 <xian_l@9hjf.com>
2017-07-27 16:09:58 +0800

    新增根据外部充值订单号查询订单信息方法

--- pwm-app/src/main/java/com/hisun/lemon/pwm/dao/IRechargeOrderDao.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/dao/IRechargeOrderDao.java
@@ -9,7 +9,10 @@ package com.hisun.lemon.pwm.dao;
 import com.hisun.lemon.framework.dao.BaseDao;
 import com.hisun.lemon.pwm.entity.RechargeOrderDO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface IRechargeOrderDao extends BaseDao<RechargeOrderDO> {
+
+	public RechargeOrderDO getRechargeOrderByExtOrderNo(@Param("extOrderNo")String extOrderNo);
 }
