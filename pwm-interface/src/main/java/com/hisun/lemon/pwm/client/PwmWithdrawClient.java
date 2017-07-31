de9c541c199261ea2d6ff42790d5cb6da73290b2
linyang <lin_y@hisuntech.com>
2017-07-31 11:09:47 +0800

    更正提现接口

--- pwm-interface/src/main/java/com/hisun/lemon/pwm/client/PwmWithdrawClient.java
+++ pwm-interface/src/main/java/com/hisun/lemon/pwm/client/PwmWithdrawClient.java
@@ -1,22 +1,28 @@
 package com.hisun.lemon.pwm.client;
 
+import com.hisun.lemon.pwm.dto.WithdrawDTO;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.hisun.lemon.framework.data.GenericDTO;
 import com.hisun.lemon.pwm.dto.WithdrawResultDTO;
 
 /**
- * 充提  提现服务接口
- * @author tone
- * @date 2017年6月27日
- * @time 下午3:06:23
+ * 提现服务接口
+ * @author leon
+ * @date 2017年7月31日
+ * @time 上午11:06:23
  *
  */
 @FeignClient("PWM")
 public interface PwmWithdrawClient {
+
+    @PostMapping("/pwm/withdraw/order")
+    public GenericDTO createOrder(@Validated @RequestBody GenericDTO<WithdrawDTO> genericWithdrawDTO);
+
     @PatchMapping("/pwm/withdraw/result")
-    public GenericDTO withdrawNotify(@Validated @RequestBody GenericDTO<WithdrawResultDTO> withdrawResultDTO);
+    public GenericDTO completeOrder(@Validated @RequestBody GenericDTO<WithdrawResultDTO> withdrawResultDTO);
 }
