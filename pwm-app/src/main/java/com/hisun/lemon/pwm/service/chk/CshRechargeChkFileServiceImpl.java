170de454ea8ed1ba166b6208a380ce2eec8714b9
童威 <tong_wei@hisuntech.com>
2017-08-17 16:27:15 +0800

    f

--- pwm-app/src/main/java/com/hisun/lemon/pwm/service/chk/CshRechargeChkFileServiceImpl.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/service/chk/CshRechargeChkFileServiceImpl.java
@@ -15,7 +15,7 @@ public class CshRechargeChkFileServiceImpl extends AbstractChkFileService {
         this.chkOrderStatus=new String[]{
                 PwmConstants.RECHARGE_ORD_S
         };
-        appCnl="CSH";
+        appCnl="PWM";
         this.lockName="PWM_CSH_CHK_FILE_LOCK";
     }
 }
