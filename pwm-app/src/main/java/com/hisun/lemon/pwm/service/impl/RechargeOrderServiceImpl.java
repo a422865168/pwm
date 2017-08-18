342bff604a4e886d3508fd6c9e73e54dce845fb9
童威 <tong_wei@hisuntech.com>
2017-08-18 18:00:05 +0800

    修改充值订单号

--- pwm-app/src/main/java/com/hisun/lemon/pwm/service/impl/RechargeOrderServiceImpl.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/service/impl/RechargeOrderServiceImpl.java
@@ -251,6 +251,8 @@ public class RechargeOrderServiceImpl implements IRechargeOrderService {
 
 		String ymd = DateTimeUtils.getCurrentDateStr();
 		String orderNo = IdGenUtils.generateId(PwmConstants.R_ORD_GEN_PRE + ymd, 15);
+		orderNo=rechargeDTO.getBusType()+ymd + orderNo;
+
 		RechargeOrderDO rechargeOrderDO = new RechargeOrderDO();
 		rechargeOrderDO.setAcTm(DateTimeUtils.getCurrentLocalDate());
 		rechargeOrderDO.setBusType(rechargeDTO.getBusType());
@@ -258,7 +260,7 @@ public class RechargeOrderServiceImpl implements IRechargeOrderService {
 		rechargeOrderDO.setOrderAmt(rechargeDTO.getAmount());
 		rechargeOrderDO.setOrderCcy("USD");
 		rechargeOrderDO.setOrderExpTm(DateTimeUtils.parseLocalDateTime("99991231235959"));
-		rechargeOrderDO.setOrderNo(ymd + orderNo);
+		rechargeOrderDO.setOrderNo(orderNo);
 		rechargeOrderDO.setOrderStatus(PwmConstants.RECHARGE_ORD_W);
 		rechargeOrderDO.setOrderTm(DateTimeUtils.getCurrentLocalDateTime());
 		rechargeOrderDO.setPsnFlag(rechargeDTO.getPsnFlag());
