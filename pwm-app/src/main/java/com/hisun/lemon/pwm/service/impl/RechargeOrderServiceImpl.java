aae35563e890688c0ce59c62eb01393b39503bc4
冼黎 <xian_l@9hjf.com>
2017-08-21 12:37:17 +0800

    添加快捷充值做账备注

--- pwm-app/src/main/java/com/hisun/lemon/pwm/service/impl/RechargeOrderServiceImpl.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/service/impl/RechargeOrderServiceImpl.java
@@ -343,7 +343,7 @@ public class RechargeOrderServiceImpl implements IRechargeOrderService {
 				// 贷：其他应付款-支付账户-xx用户现金账户
 				userAccountReqDTO=acmComponent.createAccountingReqDTO(rechargeOrderDO.getOrderNo(), rechargeOrderDO.getExtOrderNo(), rechargeOrderDO.getTxType(),
 						ACMConstants.ACCOUNTING_NOMARL, rechargeOrderDO.getOrderAmt(), null, ACMConstants.USER_AC_TYP, balCapType, ACMConstants.AC_C_FLG,
-						CshConstants.AC_ITEM_CSH_BAL, balAcNo, null, null, null, null);
+						CshConstants.AC_ITEM_CSH_BAL, balAcNo, null, null, null, "快捷充值$"+rechargeOrderDO.getOrderAmt());
 				acmComponent.requestAc(cshItemReqDTO,userAccountReqDTO);
 				break;
 			case PwmConstants.BUS_TYPE_RECHARGE_OFL:
