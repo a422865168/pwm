80e9e8382bd3d6ec2aff8e3bdf4dfc2fcab2b016
童威 <tong_wei@hisuntech.com>
2017-08-23 17:37:13 +0800

    商品描述适用语言

--- pwm-app/src/main/java/com/hisun/lemon/pwm/service/impl/RechargeOrderServiceImpl.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/service/impl/RechargeOrderServiceImpl.java
@@ -293,7 +293,15 @@ public class RechargeOrderServiceImpl implements IRechargeOrderService {
 		initCashierDTO.setAppCnl(LemonUtils.getApplicationName());
 	  	initCashierDTO.setTxType(rechargeOrderDO.getTxType());
 		initCashierDTO.setOrderAmt(rechargeDTO.getAmount());
-		initCashierDTO.setGoodsDesc("在线充值$"+rechargeDTO.getAmount());
+
+		String language=LemonUtils.getLocale().getLanguage();
+		if(StringUtils.isBlank(language)){
+			language="en";
+		}
+		String descFormat=LemonUtils.getProperty("pwm.recharge.onlineDesc."+language);
+		String desc=descFormat.replace("$amount$",String.valueOf(rechargeOrderDO.getOrderAmt()));
+
+		initCashierDTO.setGoodsDesc(desc);
 		GenericDTO<InitCashierDTO> genericDTO = new GenericDTO<>();
 		genericDTO.setBody(initCashierDTO);
 		logger.info("订单："+rechargeOrderDO.getOrderNo()+" 请求收银台");
@@ -441,7 +449,7 @@ public class RechargeOrderServiceImpl implements IRechargeOrderService {
 			hallQueryResultDTO.setUmId(userBasicInfDTO.getUserId());
 			hallQueryResultDTO.setKey(key);
 			hallQueryResultDTO.setAcBalAmt(curAcBal);
-			if(JudgeUtils.equals(PwmConstants.HALL_QUERY_TYPE_U,type)) {
+			if(JudgeUtils.equals(PwmConstants.HALL_QUERY_TYPE_U, type)) {
 				hallQueryResultDTO.setUmName(userBasicInfDTO.getUsrNm());
 			} else if (JudgeUtils.equals(PwmConstants.HALL_QUERY_TYPE_M,type)) {
 				hallQueryResultDTO.setUmName(userBasicInfDTO.getMercName());
@@ -757,7 +765,14 @@ public class RechargeOrderServiceImpl implements IRechargeOrderService {
 		initCashierDTO.setTxType(PwmConstants.TX_TYPE_RECHANGE);
 		initCashierDTO.setOrderCcy(PwmConstants.HALL_PAY_CCY);
 		initCashierDTO.setPayerName("");
-		initCashierDTO.setGoodsDesc("汇款充值$"+rechargeOrderDO.getOrderAmt());
+
+		String language=LemonUtils.getLocale().getLanguage();
+		if(StringUtils.isBlank(language)){
+			language="en";
+		}
+		String descFormat=LemonUtils.getProperty("pwm.recharge.remitDesc."+language);
+		String desc=descFormat.replace("$amount$",String.valueOf(rechargeOrderDO.getOrderAmt()));
+		initCashierDTO.setGoodsDesc(desc);
 
 		GenericDTO<InitCashierDTO> genericCashierDTO = new GenericDTO<>();
 		genericCashierDTO.setBody(initCashierDTO);
