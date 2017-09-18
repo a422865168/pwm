afc22e25d0c2bd9182f2b5be38a99ac5eb929ee5
童威 <tong_wei@hisuntech.com>
2017-09-18 17:55:02 +0800

    修改BUG

--- pwm-interface/src/main/java/com/hisun/lemon/pwm/constants/OfflineBilExtConstants.java
+++ pwm-interface/src/main/java/com/hisun/lemon/pwm/constants/OfflineBilExtConstants.java
@@ -8,17 +8,17 @@ public class OfflineBilExtConstants {
     /**
      * 汇款银行资金机构
      */
-    public final static String CRD_CORP_ORG = "crdCorpOrg";
+    public static final String CRD_CORP_ORG = "crdCorpOrg";
 
     /**
      * 公司信息备注
      */
-    public final static String PAYEE_COMPANY="payeeCompany";
+    public static final  String PAYEE_COMPANY="payeeCompany";
 
     /**
      * 收款银行账号
      */
-    public final static String BANK_ACCOUNT_NO="bankCrdNo";
+    public static final String BANK_ACCOUNT_NO="bankCrdNo";
 
 
 }
