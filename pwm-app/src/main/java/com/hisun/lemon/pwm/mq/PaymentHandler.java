298c0b016a1231dbf9f50ea43353dbf2cf17e203
冼黎 <xian_l@9hjf.com>
2017-10-13 20:39:05 +0800

    添加商户手续费

--- pwm-app/src/main/java/com/hisun/lemon/pwm/mq/PaymentHandler.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/mq/PaymentHandler.java
@@ -2,10 +2,13 @@ package com.hisun.lemon.pwm.mq;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hisun.lemon.common.utils.DateTimeUtils;
+import com.hisun.lemon.common.utils.StringUtils;
+import com.hisun.lemon.csh.enums.TradeType;
 import com.hisun.lemon.framework.stream.MultiOutput;
 import com.hisun.lemon.framework.stream.producer.Producer;
 import com.hisun.lemon.framework.stream.producer.Producers;
 import com.hisun.lemon.framework.utils.ObjectMapperHelper;
+import com.hisun.lemon.pwm.constants.PwmConstants;
 import com.hisun.lemon.pwm.entity.RechargeOrderDO;
 import com.hisun.lemon.tfm.dto.TradeFeeReqDTO;
 import org.slf4j.Logger;
@@ -49,5 +52,28 @@ public class PaymentHandler {
         String data = ObjectMapperHelper.writeValueAsString(objectMapper, tradeFeeReqDTO, true);
         logger.info("登记用户手续费写入消息队列数据：" + data);
         return tradeFeeReqDTO;
+    }
+
+    /**
+     * 登记商户手续费
+     */
+    @Producers({
+            @Producer(beanName= "merchantTradeFeeConsumer", channelName= MultiOutput.OUTPUT_THREE)
+    })
+    public TradeFeeReqDTO registMerChantFee(RechargeOrderDO orderDO,String payeeId){
+        if(StringUtils.equals(orderDO.getBusType(), PwmConstants.BUS_TYPE_RECHARGE_HALL)){
+            TradeFeeReqDTO tradeFeeReqDTO=new TradeFeeReqDTO();
+            tradeFeeReqDTO.setCcy(orderDO.getOrderCcy());
+            tradeFeeReqDTO.setUserId(payeeId);
+            tradeFeeReqDTO.setBusOrderNo(orderDO.getOrderNo());
+            tradeFeeReqDTO.setBusOrderTime(DateTimeUtils.getCurrentLocalDateTime());
+            tradeFeeReqDTO.setTradeAmt(orderDO.getOrderAmt());
+            tradeFeeReqDTO.setBusType(orderDO.getBusType());
+            String data = ObjectMapperHelper.writeValueAsString(objectMapper, tradeFeeReqDTO, true);
+            logger.info("登记商户id为:" + payeeId + "手续费写入消息队列数据：" + data);
+            return tradeFeeReqDTO;
+        }
+        logger.info("非营业厅充值订单，不登记商户手续费：" + orderDO.getOrderNo());
+        return null;
     }
 }
