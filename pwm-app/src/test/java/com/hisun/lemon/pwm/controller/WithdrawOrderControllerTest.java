0b8010a5d3630684c170c9c045d09ff675d3adbb
冼黎 <xian_l@9hjf.com>
2017-10-23 20:31:11 +0800

    注释test

--- pwm-app/src/test/java/com/hisun/lemon/pwm/controller/WithdrawOrderControllerTest.java
+++ pwm-app/src/test/java/com/hisun/lemon/pwm/controller/WithdrawOrderControllerTest.java
@@ -1,52 +1,29 @@
 package com.hisun.lemon.pwm.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.hisun.lemon.framework.data.GenericDTO;
-import com.hisun.lemon.pwm.constants.PwmConstants;
-import com.hisun.lemon.pwm.dto.WithdrawResultDTO;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.math.BigDecimal;
-
 /**
  * @author leon
  * @date 2017/7/14
  * @time 16:52
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class WithdrawOrderControllerTest {
 
-    @Autowired
-    private WebApplicationContext context;
+   // @Autowired
+   // private WebApplicationContext context;
 
 
-    @Before
-    public void setupMockMvc() throws Exception {
+   // @Before
+   // public void setupMockMvc() throws Exception {
 
-    }
+   // }
 
     /**
      * 测试生成提现订单
      * @throws Exception
      */
-    @Test
-    @Ignore
+//    @Test
+//    @Ignore
     public void testCreateOrder() throws Exception{
 
 /*        WithdrawResultDTO withdrawResultDTO = new WithdrawResultDTO();
@@ -81,8 +58,8 @@ public class WithdrawOrderControllerTest {
      * 测试提现结果处理
      * @throws Exception
      */
-    @Test
-    @Ignore
+//    @Test
+//    @Ignore
     public void testCompleteOrder() throws Exception{
 
        /* WithdrawComplDTO withdrawComplDTO = new WithdrawComplDTO();
@@ -105,8 +82,8 @@ public class WithdrawOrderControllerTest {
     }
 
 
-    @Test
-    @Ignore
+//    @Test
+//    @Ignore
     public void hcouponTest(){
 //        String descStr=getViewOrderInfo(PwmConstants.BUS_TYPE_HCOUPON,args);
 //        System.out.println(descStr);
