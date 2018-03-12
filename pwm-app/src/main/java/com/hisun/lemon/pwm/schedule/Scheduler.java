916e60efbf2dea716835ecb9ee778d304d074026
ruanlp <ruan_lp@cloudfin.com>
2018-03-12 17:12:51 +0800

    生成对账文件

--- pwm-app/src/main/java/com/hisun/lemon/pwm/schedule/Scheduler.java
+++ pwm-app/src/main/java/com/hisun/lemon/pwm/schedule/Scheduler.java
@@ -1,14 +1,15 @@
 package com.hisun.lemon.pwm.schedule;
 
-import com.hisun.lemon.framework.schedule.batch.BatchScheduled;
-import com.hisun.lemon.pwm.service.chk.AbstractChkFileService;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import com.hisun.lemon.framework.schedule.batch.BatchScheduled;
+import com.hisun.lemon.pwm.service.chk.AbstractChkFileService;
 
 /**
  * 定时任务调度器
@@ -21,8 +22,8 @@ public class Scheduler {
 	private List<AbstractChkFileService> scheduleService;
 
 
-	@BatchScheduled(cron="0 0/1 * * * ?")
-	//@BatchScheduled(cron="0 0/30 9-12 * * ?")
+	//@BatchScheduled(cron="0 0/1 * * * ?")
+	@BatchScheduled(cron="0 0/30 9-12 * * ?")
 	public void createChkFile(){
 		System.out.println("生成对账文件");
 		for(AbstractChkFileService item:scheduleService){
